f4a329ba8669f86e212bd715ce525d100e77784a
yovanmc <yovanmc@users.noreply.github.com>
2026-06-19 13:44:12 -0400

    feat(app): library page (search/filter/sort, rows+poster)

--- src/Compass.App/Views/LibraryView.xaml.cs
+++ src/Compass.App/Views/LibraryView.xaml.cs
@@ -1,11 +1,30 @@
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Compass.App.ViewModels;
 
 namespace Compass.App.Views;
 
 public partial class LibraryView : Page
 {
-    public LibraryView()
+    private readonly LibraryViewModel _vm;
+
+    public LibraryView(LibraryViewModel vm)
     {
         InitializeComponent();
+        _vm = vm;
+        DataContext = vm;
+    }
+
+    // Toggle buttons act as a two-button radio group for the view mode.
+    private void RowViewButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        _vm.IsPosterView = false;
+        if (sender is ToggleButton tb) tb.IsChecked = true;
+    }
+
+    private void PosterViewButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        _vm.IsPosterView = true;
+        if (sender is ToggleButton tb) tb.IsChecked = true;
     }
 }
